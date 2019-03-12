ba74f0e74d72dccd0fd83cd4ae5bbb7eebf7837f
Lucía <luciamonterog24@gmail.com>
2019-03-12 12:28:10 +0100

    metodo multiplicar

--- src/Util.java
+++ src/Util.java
@@ -1,11 +1,27 @@
 public class Util {
+	
+	public static void main(String[]args) {
+		
+		int num1 = 6;
+		int num2 = 8;
+		
+		suma(num1, num2);
+		multiplicar(num1, num2);
+		
+	}
 
-	public void suma(int num1, int num2) {
+	public static void suma(int num1, int num2) {
 		
 		System.out.println("Suma");
 		
 		System.out.println(num1+num2);
 
+	}
+	
+	public static void multiplicar(int num1, int num2) {
+		
+		System.out.println(num1+"x"+num2+"="+(num1*num2));
+		
 	}
 	
 }
