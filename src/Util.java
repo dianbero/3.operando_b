04e11f17d1bece4be86ebaafc3a28a5abb4fa42d
Alvaro <sesilva.93@gmail.com>
2019-03-12 12:13:01 +0100

    New change

--- src/Util.java
+++ src/Util.java
@@ -1,10 +1,10 @@
 public class Util {
 
-	public void suma(String[] args) {
+	public void suma(int num1, int num2) {
 		
 		System.out.println("Suma");
 		
-		System.out.println(2+2);
+		System.out.println(num1+num2);
 
 	}
 	
